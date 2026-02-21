331a129b32824e63553909d87b651766e61bef84
Ke'Shawn Alexander <skyspotinfo@gmail.com>
2026-02-20 20:04:58 -0500

    Updated page routing

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,25 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { RouterProvider } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { PlateRackScreen } from './screens/PlateRackScreen';
-import { LoadScreen } from './screens/LoadScreen';
-import { TransitionScreen } from './screens/TransitionScreen';
+import { createBrowserRouter } from "react-router-dom";
+import { PlateRackScreen } from "./screens/PlateRackScreen";
+import { LoadScreen } from "./screens/LoadScreen";
+import { TransitionScreen } from "./screens/TransitionScreen";
 
 export const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: LoadScreen,
-  },
-  {
-    path: '/load',
-    Component: LoadScreen,
-  },
-  {
-    path: '/transition',
-    Component: TransitionScreen,
-  },
-  {
-    path: '/rack',
-    Component: PlateRackScreen,
-  },
+  { path: "/", Component: LoadScreen },
+  { path: "/load", Component: LoadScreen },
+  { path: "/transition", Component: TransitionScreen },
+  { path: "/rack", Component: PlateRackScreen },
 ]);
