531de69c5548afb6b243d5b6eee29d6c1393e609
Ke'Shawn Alexander <skyspotinfo@gmail.com>
2026-02-20 20:39:40 -0500

    Updated routes file

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -4,8 +4,8 @@ import { LoadScreen } from "./screens/LoadScreen";
 import { TransitionScreen } from "./screens/TransitionScreen";
 
 export const router = createBrowserRouter([
-  { path: "/", Component: LoadScreen },
-  { path: "/load", Component: LoadScreen },
-  { path: "/transition", Component: TransitionScreen },
-  { path: "/rack", Component: PlateRackScreen },
+  { path: "/", element: <LoadScreen /> },
+  { path: "/load", element: <LoadScreen /> },
+  { path: "/transition", element: <TransitionScreen /> },
+  { path: "/rack", element: <PlateRackScreen /> },
 ]);
